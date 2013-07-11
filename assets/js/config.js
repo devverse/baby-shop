61a31ef824eab38a5ee87b85a307cc89694c3cdb
Robbins Cleozier <rcleozier@gmail.com>
2013-07-11 10:01:09 -0400

    Updated config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,5 +3,5 @@ var url = window.location.href;
 if (url.indexOf("localhost") != -1) {
 	var serviceURL = "http://localhost/dev/app/site2/public/mobile/";
 } else{
-	var serviceURL = "http://.com/public/mobile/";
+	var serviceURL = "http://devverse.com/public/mobile/";
 }
