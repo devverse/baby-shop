4d462893f29565fc88fc478deba8e679bc6fbd88
Robbins Cleozier <rcleozier@gmail.com>
2013-10-26 17:51:57 -0400

    Updated app styles

---
+++ app/config.js
@@ -0,0 +1,10 @@
+var url = window.location.href;
+
+if (url.indexOf("localhost") != -1) {
+	var serviceURL = "http://localhost/dev/devverse/public/infantshop/";
+} else{
+	var serviceURL = "http://devverse.com/public/running/";
+}
+
+var app_name = "The Infant Shop";
+var page_title = "The Infant Shop";
--- assets/js/config.js
+++
@@ -1,8 +0,0 @@
-var url = window.location.href;
-
-if (url.indexOf("localhost") != -1) {
-	var serviceURL = "http://localhost/dev/app/site2/public/mobile/";
-} else{
-	var serviceURL = "http://devverse.com/public/mobile/";
-}
-
