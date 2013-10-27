6b6178b6831c12ec02db7ee3200f1f02effac9b5
Robbins Cleozier <rcleozier@gmail.com>
2013-10-26 22:42:01 -0400

    Update app

--- app/config.js
+++ app/config.js
@@ -3,7 +3,7 @@ var url = window.location.href;
 if (url.indexOf("localhost") != -1) {
 	var serviceURL = "http://localhost/dev/devverse/public/infantshop/";
 } else{
-	var serviceURL = "http://devverse.com/public/running/";
+	var serviceURL = "http://devverse.com/public/infantshop/";
 }
 
 var app_name = "The Infant Shop";
